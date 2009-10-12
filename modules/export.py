ad0faa8c394d4fcc6763aec04c00509da2170f10
jorge.mordsong <jorge.mordsong@e1b84198-80da-11de-9ae6-97cb6de55c0b>
2009-10-12 18:18:45 +0000

    añadida funcion para generar un html con los datos de la base de datos y solucionado error para generar el xml

--- modules/export.py
+++ modules/export.py
@@ -48,10 +48,40 @@ def generar_xml():
             xml=xml+u"</dia>\n<dia>\n\t<fecha>"+str(fecha)+u"</fecha>\n"
         xml=xml+u"\t<"+str(datos.col(2))+u">"+str(datos.col(3))+u"</"+str(datos.col(2))+u">\n"
         datos.next_line()
-    xmlFinal=u"<?xml version=\"1.0\" encoding=\"UTF-8\"?>"+xml[6:]+u"</dia>"
+    xmlFinal=u"<?xml version=\"1.0\" encoding=\"UTF-8\"?><root>"+xml[6:]+u"</dia></root>"
     fichero=codecs.open(unidad+':\\Python\\resources\db\db.xml','w','utf8')
     fichero.write(xmlFinal)
+
+def generar_html():
+    global unidad
     
+    html=u""
+    cabecerahtml=u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"><html><head><title>CSDs60WebAnalyzer</title>"
+    css=u"<style type=\"text/css\">#datosCont{margin:20px auto 0;padding:15px 20px;width:700px}div.dat\
+osDia{margin-bottom:15px}div.datosDiaTitulo{border-bottom:2px solid #97d25d;cursor:pointer;font-we\
+ight:700}div.datosDiaCont{display:none;padding-left:20px}div.datosDiaContAtr{float:left;width:70%}\
+div.datosDiaContVal{float:right;text-align:center;width:30%}</style>"
+    javascript=u"<script type=\"text/javascript\">function mostrar(act){var hijos=act.parentNode.child\
+Nodes;for(var i=0;i<hijos.length;i++){if(hijos[i].tagName==\"DIV\"&&hijos[i].className==\"datosDia\
+Cont\"){if(hijos[i].style.display==\"block\"){hijos[i].style.display=\"none\"}else{hijos[i].style.\
+display=\"block\"}}}}window.onload=function(){var dias=document.getElementById(\"datosCont\").chil\
+dNodes;for(var i=0;i<dias.length;i++){if(dias[i].tagName==\"DIV\"&&dias[i].className==\"datosDia\"\
+){for(var j=0;j<dias[i].childNodes.length;j++){if(dias[i].childNodes[j].tagName==\"DIV\"&&dias[i].\
+childNodes[j].className==\"datosDiaTitulo\"){var diaValor=dias[i].childNodes[j];var fecha=new Date\
+();fecha.setTime(diaValor.innerHTML*1000);diaValor.innerHTML=fecha.getDate()+\"/\"+(fecha.getMonth(\
+)+1)+\"/\"+fecha.getFullYear();dias[i].childNodes[j].onclick=function(){mostrar(this)}}}}}}</script>"
+    datos=base_de_datos.obtener_datos_diario()
+    fecha=0
+    for i in range(datos.count_line()):
+        datos.get_line()
+        if fecha!=datos.col(1):
+            fecha=datos.col(1)
+            html=html+u"</div></div><div class=\"datosDia\"><div class=\"datosDiaTitulo\">"+str(fecha)+"</div><div class=\"datosDiaCont\">"
+        html=html+u"<div class=\"datosDiaContAtr\">"+str(datos.col(2))+u"</div><div class=\"datosDiaContVal\">"+str(datos.col(3))+u"</div>"
+        datos.next_line()
+    htmlFinal=cabecerahtml+css+javascript+u"</head><body><div id=\"datosCont\">"+html[12:]+u"</div></body></html>"
+    fichero=codecs.open(unidad+':\\Python\\resources\db\datos.html','w','utf8')
+    fichero.write(htmlFinal)
 
 def handle_redraw(rect):
     global canvasExport
@@ -86,3 +116,4 @@ def mostrarExport(vAtras):
     else:
         appuifw.app.exit_key_handler=volverAtras
     generar_xml()
+    generar_html()
