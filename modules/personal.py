0abc2d0146921cfdc2179923f44fe7cd81513a78
jorge.mordsong <jorge.mordsong@e1b84198-80da-11de-9ae6-97cb6de55c0b>
2009-12-23 22:06:06 +0000

    arreglada la formula para calcular dosis de insulina y añadidas mejoras

--- modules/personal.py
+++ modules/personal.py
@@ -71,13 +71,13 @@ def handle_redraw(rect):
     canvasPersonal.text((250,205),u"%03d u"%datos[2],colorTexto[2],font=(u"legend",17))
     canvasPersonal.line((20,225,330,225),0)
     canvasPersonal.text((30,240),getLang(u"RATD"),0x000000,font=(u"legend",17,appuifw.STYLE_BOLD))
-    canvasPersonal.text((250,240),u"%02.2f"%datos[3],colorTexto[3],font=(u"legend",17))
+    canvasPersonal.text((250,240),u"%02d ml"%datos[3],colorTexto[3],font=(u"legend",17))
     canvasPersonal.line((20,260,330,260),0)
     canvasPersonal.text((30,275),getLang(u"RATA"),0x000000,font=(u"legend",17,appuifw.STYLE_BOLD))
-    canvasPersonal.text((250,275),u"%02.2f"%datos[4],colorTexto[4],font=(u"legend",17))
+    canvasPersonal.text((250,275),u"%02d ml"%datos[4],colorTexto[4],font=(u"legend",17))
     canvasPersonal.line((20,295,330,295),0)
     canvasPersonal.text((30,310),getLang(u"RATC"),0x000000,font=(u"legend",17,appuifw.STYLE_BOLD))
-    canvasPersonal.text((250,310),u"%02.2f"%datos[5],colorTexto[5],font=(u"legend",17))
+    canvasPersonal.text((250,310),u"%02d ml"%datos[5],colorTexto[5],font=(u"legend",17))
     canvasPersonal.line((20,330,330,330),0)
 
 def volverAtras():
@@ -100,11 +100,11 @@ def press_select():
         elif movimientos[actPos][2] == u"totalinsu":
             base_de_datos.actualizar_totalinsu(datos[actPos])
         elif movimientos[actPos][2] == u"ratd":
-            base_de_datos.actualizar_ratiodesayuno(datos[actPos])
+            base_de_datos.actualizar_insudesayuno(datos[actPos])
         elif movimientos[actPos][2] == u"rata":
-            base_de_datos.actualizar_ratioalmuerzo(datos[actPos])
+            base_de_datos.actualizar_insualmuerzo(datos[actPos])
         else:
-            base_de_datos.actualizar_ratiocena(datos[actPos])
+            base_de_datos.actualizar_insucena(datos[actPos])
         #print datos[actPos]
         actMod=False
     else:        
@@ -137,6 +137,9 @@ def moverCursor(desppeso,desp,despfloat,pos):
             elif actPos == 2:
                 if datos[actPos]<1:
                     datos[actPos]=1
+            elif actPos == 3 or actPos == 4 or actPos == 5:
+                if datos[actPos]<0:
+                    datos[actPos]=0
             else:
                 if datos[actPos]<0.01:
                     datos[actPos]=0.01
@@ -145,16 +148,16 @@ def moverCursor(desppeso,desp,despfloat,pos):
     appuifw.app.body = canvasPersonal
     
 def press_up():
-    moverCursor(5,50,1,0)
+    moverCursor(5,50,5,0)
 
 def press_right():
-    moverCursor(0.01,1,0.01,1)
+    moverCursor(0.01,1,1,1)
 
 def press_down():
-    moverCursor(-5,-50,-1,2)
+    moverCursor(-5,-50,-5,2)
 
 def press_left():
-    moverCursor(-0.01,-1,-0.01,3)
+    moverCursor(-0.01,-1,-1,3)
 
 def mostrar_personal(vAtras):
     global movimientos
@@ -162,9 +165,9 @@ def mostrar_personal(vAtras):
         [0,[0,0,1,0],u"peso",400.00],
         [1,[-1,0,1,0],u"altura",250],
         [2,[-1,0,1,0],u"totalinsu",200],
-        [3,[-1,0,1,0],u"ratd",10.00],
-        [4,[-1,0,1,0],u"rata",10.00],
-        [5,[-1,0,0,0],u"ratc",10.00]
+        [3,[-1,0,1,0],u"ratd",99],
+        [4,[-1,0,1,0],u"rata",99],
+        [5,[-1,0,0,0],u"ratc",99]
         ]
     global datos
     datos=[0 for x in range(6)]
@@ -186,11 +189,11 @@ def mostrar_personal(vAtras):
         #ratioAlmuerzo = 1.34
         #ratioCena = 2
         elif i == 3:            
-            datos[i] = base_de_datos.obtener_ratiodesayuno_actual()
+            datos[i] = base_de_datos.obtener_insudesayuno_actual()
         elif i == 4:
-            datos[i] = base_de_datos.obtener_ratioalmuerzo_actual()
+            datos[i] = base_de_datos.obtener_insualmuerzo_actual()
         else:
-            datos[i] = base_de_datos.obtener_ratiocena_actual()
+            datos[i] = base_de_datos.obtener_insucena_actual()
     global actPos
     actPos=0
     global actMod
